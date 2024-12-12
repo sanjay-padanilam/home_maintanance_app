07e65e82c592c284fcddadf3bf2184d1ae492422
sanjay-padanilam <sanjaysaji193@gmail.com>
2024-12-12 18:23:52 +0530

    firehbase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:home_maintanance_app/firebase_options.dart';
 import 'package:home_maintanance_app/view/homescreen/homescreen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
