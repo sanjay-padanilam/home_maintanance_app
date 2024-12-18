fb41ba4587c2edc25ebd86f13afc14618db5acad
sanjay-padanilam <sanjaysaji193@gmail.com>
2024-12-18 10:34:47 +0530

    ui set

--- lib/view/homescreen/homescreen.dart
+++ lib/view/homescreen/homescreen.dart
@@ -1,11 +1,6 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:home_maintanance_app/dummydb.dart';
-import 'package:home_maintanance_app/view/homescreen/widgets/Service_tile.dart';
-import 'package:home_maintanance_app/view/homescreen/widgets/recentwork_card.dart';
-import 'package:home_maintanance_app/view/service_Details_Screen/service_details_Screen.dart';
 
 class Homescreen extends ConsumerStatefulWidget {
   const Homescreen({super.key});
@@ -18,141 +13,197 @@ class _HomescreenState extends ConsumerState<Homescreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final Stream<QuerySnapshot> _servicesStream =
       FirebaseFirestore.instance.collection('services').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.sizeOf(context);
-    return Scaffold(
-        key: _scaffoldKey,
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text(
-                  'Menu',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Home'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  _scaffoldKey.currentState?.openDrawer();
-                },
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                ),
-              ),
-            )
-          ],
-          backgroundColor: Colors.black,
-          title: Text(
-            'Service App',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: StreamBuilder(
-          stream: _servicesStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Text('Something went wrong');
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator.adaptive(),
-              );
-            }
+    // Get screen dimensions
+    final screenWidth = MediaQuery.of(context).size.width;
+    // final screenHeight = MediaQuery.of(context).size.height;
 
-            return ListView(
-              children: [
-                // Services Section
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Our Services',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
+    // Determine grid column count based on screen width
+    final gridCrossAxisCount = screenWidth > 600 ? 3 : 2;
+
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
                 ),
-                Container(
-                    height: 100,
-                    child: GridView.builder(
-                      itemCount: Dummydb.services.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 4),
-                      itemBuilder: (context, index) => InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ServiceDetailsScreen(
-                                  serviceName: Dummydb.services[index]['name'],
-                                  serviceIcon: Dummydb.services[index]['icon'],
-                                  serviceDescription: Dummydb.services[index]
-                                      ['description'],
-                                  features: List<String>.from(
-                                      Dummydb.services[index]['features']),
-                                ),
-                              ));
-                        },
-                        child: ServiceTile(Dummydb.services[index]["name"],
-                            Dummydb.services[index]["icon"]),
-                      ),
-                    )),
-                // Top Deals Slider
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CarouselSlider(
-                      items: List.generate(
-                        10,
-                        (index) => Container(
-                          height: size.height / 2,
-                          width: size.width * 2 / 2.4,
-                          color: Colors.black,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          "Dashboard",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+        elevation: 0,
+      ),
+      body: StreamBuilder(
+        stream: _servicesStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong'),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+
+          // Null check for snapshot.data
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text('No services available'),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.deepPurple.shade300,
+                    Colors.deepPurple.shade100
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Header Row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Welcome, Sanjay",
+                        style: TextStyle(
+                          fontSize: screenWidth > 400
+                              ? 24
+                              : 18, // Responsive font size
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                      options: CarouselOptions(
-                          autoPlay: true, enlargeCenterPage: true)),
-                ),
-                // Recent Work Ratings Section
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Recent Work & Ratings',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      CircleAvatar(
+                        radius: screenWidth > 400
+                            ? 24
+                            : 16, // Responsive avatar size
+                        backgroundImage:
+                            AssetImage('assets/profile_placeholder.png'),
+                      ),
+                    ],
                   ),
-                ),
-                RecentWorkTile('John Doe', 'Electrician', 4.5),
-                RecentWorkTile('Jane Smith', 'Plumber', 4.7),
-                RecentWorkTile('Robert Brown', 'Cleaner', 4.0),
-                RecentWorkTile('Emily White', 'Gardener', 4.8),
-              ],
+                  SizedBox(height: 20),
+                  Text(
+                    "Our Services.....",
+                    style: TextStyle(
+                      fontSize:
+                          screenWidth > 400 ? 24 : 30, // Responsive font size
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  // Responsive Grid
+                  Expanded(
+                    child: GridView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: gridCrossAxisCount,
+                        crossAxisSpacing: 12,
+                        mainAxisSpacing: 12,
+                        childAspectRatio:
+                            screenWidth > 600 ? 1.2 : 1, // Adjust card ratio
+                      ),
+                      itemBuilder: (context, index) {
+                        final servicelist = snapshot.data!.docs;
+
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          elevation: 5,
+                          color: Colors.white,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 20,
+                                top: 20,
+                                child: Container(
+                                  height: screenWidth > 400
+                                      ? 80
+                                      : 60, // Responsive size
+                                  width: screenWidth > 400 ? 80 : 60,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: NetworkImage(
+                                            servicelist[index]['image'])),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                top: screenWidth > 400 ? 120 : 90,
+                                left: 30,
+                                child: Text(
+                                  servicelist[index]['name'],
+                                  style: TextStyle(
+                                    fontSize: screenWidth > 400
+                                        ? 16
+                                        : 12, // Responsive text size
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             );
-          },
-        ));
+          }
+          return SizedBox();
+        },
+      ),
+    );
   }
 }
