db59da1d851cf47a75bb76b187f4a88e8f47e9a5
sanjay-padanilam <sanjaysaji193@gmail.com>
2024-12-30 16:43:51 +0530

    set

--- lib/view/service_cart/service_cart.dart
+++ lib/view/service_cart/service_cart.dart
@@ -15,75 +15,99 @@ class ServiceCart extends ConsumerStatefulWidget {
 
 class ServiceCartState extends ConsumerState<ServiceCart> {
   final User? user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.deepPurple.shade200,
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurple.shade400,
-          title: Text('Order List'),
-        ),
-        body: StreamBuilder<List<OrderModel>>(
-          stream: ref
-              .read(ServiceCartStateProvider.notifier)
-              .fetchUserOrders(user!.uid.toString()),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            }
-            if (snapshot.hasError || !snapshot.hasData) {
-              return Text('Error fetching orders ${snapshot.error}');
-            }
+      backgroundColor: Colors.deepPurple.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple.shade400,
+        title: Text('Order List'),
+      ),
+      body: StreamBuilder<List<OrderModel>>(
+        stream: ref
+            .read(ServiceCartStateProvider.notifier)
+            .fetchUserOrders(user!.uid.toString()),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError || !snapshot.hasData) {
+            return Center(
+                child: Text('Error fetching orders: ${snapshot.error}'));
+          }
 
-            final orders = snapshot.data!;
-            return ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (context, index) {
-                final order = orders[index];
-                log(order.toString());
+          final orders = snapshot.data!;
+          return ListView.builder(
+            itemCount: orders.length,
+            itemBuilder: (context, index) {
+              final order = orders[index];
+              log(order.toString());
 
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      ListTile(
-                        title: Text(
-                          ' ${order.servicename.toString()}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: order.status.toLowerCase() == 'approved'
-                            ? Container(
-                                color: Colors.green,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text("Order Approved"),
-                                ),
-                              )
-                            : order.status.toLowerCase() == 'rejected'
-                                ? Container(
-                                    color: Colors.red,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text("Order Rejected"),
-                                    ),
-                                  )
-                                : Container(
-                                    color: Colors.yellow,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text("Order pending"),
-                                    ),
-                                  ),
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        order.servicename.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      Divider(
-                        height: 12,
-                      )
-                    ],
-                  ),
-                );
-              },
-            );
-          },
-        ));
+                      trailing: order.status.toLowerCase() == 'approved'
+                          ? Container(
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Order Approved",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            )
+                          : order.status.toLowerCase() == 'rejected'
+                              ? Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Order Rejected",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                )
+                              : Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.yellow,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Order Pending",
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                ),
+                    ),
+                    SizedBox(height: 8),
+                    if (order.status.toLowerCase() == 'pending')
+                      Text(
+                        'Please wait while we process your order.',
+                        style: TextStyle(color: Colors.black54),
+                      ),
+                    if (order.status.toLowerCase() == 'approved')
+                      Text(
+                        'Our executive will contact you soon.',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    Divider(height: 12),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
